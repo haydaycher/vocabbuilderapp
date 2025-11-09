7f36e57c190dbd0203c5b72dd8c3a83a66d9df98
Yevheniia Cherepashchuk <chtozalogin.jh@gmail.com>
2025-11-09 16:20:37 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: ".", // явно корінь
+  build: {
+    outDir: "dist", // папка для збірки
+  },
 });
